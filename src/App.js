29ba31108161cc4594f04f3d83a39019578ae28f
Josh Caldwell <josh@devpipeline.com>
2024-04-11 15:58:30 -0600

    trying to figure out browser router stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React, { useState, useEffect } from "react";
 import Calendar from "./pages/Calendar";
 import Navbar from "./components/Navbar";
 import "./styles/App.scss";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Calendar />
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Calendar />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
